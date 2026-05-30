e23ac45a15ed400d50351df0ec0c0d4ffdc60821
4booser <4booser@gmail.com>
2026-05-30 18:04:29 +0300

    Backup

---
+++ src/repository.cpp
@@ -0,0 +1,14 @@
+#include "models.h"
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json
+
+void SaveTeacher(Teacher teacher){
+    json data ={
+        {"Id", teacher.Id},
+        {"FullName", teacher.FullName},
+        {"DigitalComission", teacher.DigitialComission},
+        {"Quota", teacher.Quota}
+    };
+    data.dump(2);
+}
