97a418cd09c48f434ca94356e46f9bb58045a4ef
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 17:56:06 +0300

    Return operation result from group repository mutations

--- include/repositories/group_repository.h
+++ include/repositories/group_repository.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include "common/operation_result.h"
 #include "models.h"
 #include <vector>
 #include <optional>
 
 unsigned short GetLastGroupId();
 
-bool SaveGroup(models::Group& group);
+OperationResult SaveGroup(models::Group& group);
 std::vector<models::Group> GetGroups();
 std::optional<models::Group> GetGroupById(unsigned short id);
-bool EditGroupById(const unsigned short& id, const models::Group& updated_group);
-bool DeleteGroupById(const unsigned short& id);
+OperationResult EditGroupById(const unsigned short& id, const models::Group& updated_group);
+OperationResult DeleteGroupById(const unsigned short& id);
