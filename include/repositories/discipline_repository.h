a8a12013790682a0bb6009d14eeb8dc8ea7b2bf0
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 15:08:12 +0300

    Update discipline repository declarations

--- include/repositories/discipline_repository.h
+++ include/repositories/discipline_repository.h
@@ -2,11 +2,11 @@
 
 #include "models.h"
 #include <vector>
+#include <optional>
 
 unsigned short GetLastDisciplineId();
 
 bool SaveDiscipline(models::Discipline& discipline);
 std::vector<models::Discipline> GetDisciplines();
-bool EditDiscipline(unsigned short& id);
-bool DeleteDiscipline(unsigned short& id);
-
+std::optional<models::Discipline> GetDisciplineById(unsigned short id);
+bool EditDisciplineById(const unsigned short& id, const models::Discipline& updated_discipline);
