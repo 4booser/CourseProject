f7b0ab8072c64347575b00727fc5dac8c9df4245
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 15:34:11 +0300

    Initialize model fields and rename workload discipline id

--- include/models.h
+++ include/models.h
@@ -4,36 +4,35 @@
 
 namespace models{
     struct Teacher{
-    unsigned short id;
-    std::string full_name;
-    std::string digital_commission;
-    unsigned int quota; // in hours
+        unsigned short id = 0;
+        std::string full_name;
+        std::string digital_commission;
+        unsigned int quota = 0; // in hours
     };
 
     struct Group{
-    unsigned short id;
-    std::string name;
-    unsigned short course;
-    std::string speciality;
+        unsigned short id = 0;
+        std::string name;
+        unsigned short course = 0;
+        std::string speciality;
     };
 
     struct Discipline{
-    unsigned short id;
-    std::string name;
-    unsigned int quota; // in hours
+        unsigned short id = 0;
+        std::string name;
+        unsigned int quota = 0; // in hours
     }; 
 
     struct Workload{
-    unsigned short id;
-    std::vector<unsigned short> teacher_ids;
-    unsigned short subject_id;
-    std::vector<unsigned short> group_ids;
-    unsigned int lectures;
-    unsigned int practical_classes;
-    unsigned int laboratory_classes;
-    unsigned int seminars;
-    unsigned int consultations;
-    unsigned int total_hours;
+        unsigned short id = 0;
+        std::vector<unsigned short> teacher_ids;
+        unsigned short discipline_id = 0;
+        std::vector<unsigned short> group_ids;
+        unsigned int lectures = 0;
+        unsigned int practical_classes = 0;
+        unsigned int laboratory_classes = 0;
+        unsigned int seminars = 0;
+        unsigned int consultations = 0;
+        unsigned int total_hours = 0;
     };
 }
-
