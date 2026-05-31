56ac33b5f321ce66e361a08ebdbeec5208d0a837
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 18:11:39 +0300

    Return operation result from discipline repository mutations

--- include/repositories/discipline_repository.h
+++ include/repositories/discipline_repository.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include "common/operation_result.h"
 #include "models.h"
 #include <vector>
 #include <optional>
 
 unsigned short GetLastDisciplineId();
 
-bool SaveDiscipline(models::Discipline& discipline);
+OperationResult SaveDiscipline(models::Discipline& discipline);
 std::vector<models::Discipline> GetDisciplines();
 std::optional<models::Discipline> GetDisciplineById(unsigned short id);
-bool EditDisciplineById(const unsigned short& id, const models::Discipline& updated_discipline);
-bool RemoveDisciplineById(const unsigned short& id);
+OperationResult EditDisciplineById(const unsigned short& id, const models::Discipline& updated_discipline);
+OperationResult RemoveDisciplineById(const unsigned short& id);
