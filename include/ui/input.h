273be857b92fa4774e2fdf2c92899cd3db96cebb
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 16:09:48 +0300

    Add common line input declarations

--- include/ui/input.h
+++ include/ui/input.h
@@ -5,6 +5,8 @@
 
 namespace ui
 {
+    void ClearInputLine();
+    std::string ReadLine(const std::string& prompt);
     unsigned int ReadUnsignedInt(const std::string& prompt);
     unsigned short ReadUnsignedShort(const std::string& prompt);
     std::vector<unsigned short> ReadUnsignedShortList(const std::string& prompt);
