a591d855a278e65dbb1b0c8e6ad1806fb67e1b79
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 15:18:58 +0300

    Add workload remove declaration

--- include/repositories/workload_repository.h
+++ include/repositories/workload_repository.h
@@ -10,3 +10,4 @@ bool SaveWorkload(models::Workload& workload);
 std::vector<models::Workload> GetWorkloads();
 std::optional<models::Workload> GetWorkloadById(unsigned short id);
 bool EditWorkloadById(const unsigned short& id, const models::Workload& updated_workload);
+bool RemoveWorkloadById(const unsigned short& id);
