87d4910b2deac03e7eaeac2a1212d15613d6b36f
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 19:37:45 +0300

    Extract shared CRUD menu handler

--- src/ui/menu_handlers.cpp
+++ src/ui/menu_handlers.cpp
@@ -8,118 +8,94 @@
 #include "ui/input.h"
 
 #include <iostream>
+#include <string>
+
+namespace
+{
+    using CreateHandler = void (*)();
+    using PrintHandler = void (*)();
+    using EditHandler = void (*)(const unsigned short&);
+    using DeleteHandler = void (*)(const unsigned short&);
+
+    void HandleCrudMenu(
+        const std::string& edit_prompt,
+        const std::string& delete_prompt,
+        CreateHandler create_handler,
+        PrintHandler print_handler,
+        EditHandler edit_handler,
+        DeleteHandler delete_handler)
+    {
+        ui::ShowOptions();
+        unsigned short option = ui::ReadUnsignedShort("");
+
+        switch (option)
+        {
+            case 1:
+                create_handler();
+                break;
+            case 2:
+                print_handler();
+                break;
+            case 3:
+                print_handler();
+                edit_handler(ui::ReadUnsignedShort(edit_prompt));
+                break;
+            case 4:
+                print_handler();
+                delete_handler(ui::ReadUnsignedShort(delete_prompt));
+                break;
+            case 0:
+                break;
+            default:
+                std::cout << "Невiрна опцiя.\n";
+                break;
+        }
+    }
+}
 
 namespace ui
 {
     void HandleTeacherMenu()
     {
-        ShowOptions();
-        unsigned short option = ReadUnsignedShort("");
-
-        switch(option){
-            case 1:
-                HandleTeacherCreate();
-                break;
-            case 2:
-                HandleTeachersPrint();
-                break;
-            case 3:
-                HandleTeachersPrint();
-                HandleTeacherEdit(ReadUnsignedShort("Введiть Id викладача для редагування: "));
-                break;
-            case 4:
-                HandleTeachersPrint();
-                HandleTeacherDelete(ReadUnsignedShort("Введiть Id викладача для видалення: "));
-                break;
-            case 0:
-                break;
-            default:
-                std::cout << "Невiрна опцiя.\n";
-                break;
-        }
+        HandleCrudMenu(
+            "Введiть Id викладача для редагування: ",
+            "Введiть Id викладача для видалення: ",
+            HandleTeacherCreate,
+            HandleTeachersPrint,
+            HandleTeacherEdit,
+            HandleTeacherDelete);
     }
 
     void HandleDisciplineMenu()
     {
-        ShowOptions();
-        unsigned short option = ReadUnsignedShort("");
-
-        switch(option){
-            case 1:
-                HandleDisciplineCreate();
-                break;
-            case 2:
-                HandleDisciplinesGet();
-                break;
-            case 3:
-                HandleDisciplinesGet();
-                HandleDisciplineEdit(ReadUnsignedShort("Введiть Id дисциплiни для редагування: "));
-                break;
-            case 4:
-                HandleDisciplinesGet();
-                HandleDisciplineDelete(ReadUnsignedShort("Введiть Id дисциплiни для видалення: "));
-                break;
-            case 0:
-                break;
-            default:
-                std::cout << "Невiрна опцiя.\n";
-                break;
-        }
+        HandleCrudMenu(
+            "Введiть Id дисциплiни для редагування: ",
+            "Введiть Id дисциплiни для видалення: ",
+            HandleDisciplineCreate,
+            HandleDisciplinesGet,
+            HandleDisciplineEdit,
+            HandleDisciplineDelete);
     }
 
     void HandleGroupMenu()
     {
-        ShowOptions();
-        unsigned short option = ReadUnsignedShort("");
-
-        switch(option){
-            case 1:
-                HandleGroupCreate();
-                break;
-            case 2:
-                HandleGroupsGet();
-                break;
-            case 3:
-                HandleGroupsGet();
-                HandleGroupEdit(ReadUnsignedShort("Введiть Id групи для редагування: "));
-                break;
-            case 4:
-                HandleGroupsGet();
-                HandleGroupDelete(ReadUnsignedShort("Введiть Id групи для видалення: "));
-                break;
-            case 0:
-                break;
-            default:
-                std::cout << "Невiрна опцiя.\n";
-                break;
-        }
+        HandleCrudMenu(
+            "Введiть Id групи для редагування: ",
+            "Введiть Id групи для видалення: ",
+            HandleGroupCreate,
+            HandleGroupsGet,
+            HandleGroupEdit,
+            HandleGroupDelete);
     }
 
     void HandleWorkloadMenu()
     {
-        ShowOptions();
-        unsigned short option = ReadUnsignedShort("");
-
-        switch(option){
-            case 1:
-                HandleWorkloadCreate();
-                break;
-            case 2:
-                HandleWorkloadsGet();
-                break;
-            case 3:
-                HandleWorkloadsGet();
-                HandleWorkloadEdit(ReadUnsignedShort("Введiть Id навантаження для редагування: "));
-                break;
-            case 4:
-                HandleWorkloadsGet();
-                HandleWorkloadDelete(ReadUnsignedShort("Введiть Id навантаження для видалення: "));
-                break;
-            case 0:
-                break;
-            default:
-                std::cout << "Невiрна опцiя.\n";
-                break;
-        }
+        HandleCrudMenu(
+            "Введiть Id навантаження для редагування: ",
+            "Введiть Id навантаження для видалення: ",
+            HandleWorkloadCreate,
+            HandleWorkloadsGet,
+            HandleWorkloadEdit,
+            HandleWorkloadDelete);
     }
 }
