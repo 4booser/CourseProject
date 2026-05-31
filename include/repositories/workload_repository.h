c2ccb3577a4d01f88eb9e9d7d307eb269c0dcf77
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 15:54:16 +0300

    Add workload relation check declarations

--- include/repositories/workload_repository.h
+++ include/repositories/workload_repository.h
@@ -11,3 +11,7 @@ std::vector<models::Workload> GetWorkloads();
 std::optional<models::Workload> GetWorkloadById(unsigned short id);
 bool EditWorkloadById(const unsigned short& id, const models::Workload& updated_workload);
 bool RemoveWorkloadById(const unsigned short& id);
+
+bool HasWorkloadForTeacher(unsigned short teacher_id);
+bool HasWorkloadForGroup(unsigned short group_id);
+bool HasWorkloadForDiscipline(unsigned short discipline_id);
