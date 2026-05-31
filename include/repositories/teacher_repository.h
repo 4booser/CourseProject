b70ae9337ed8600e85c44e55d4eceda6243ed97b
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 17:09:04 +0300

    Return operation result from teacher repository mutations

--- include/repositories/teacher_repository.h
+++ include/repositories/teacher_repository.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "common/operation_result.h"
 #include "models.h"
 #include <vector>
 #include <optional>
 
 unsigned short GetLastTeacherId();
 
-bool SaveTeacher(models::Teacher& teacher);
+OperationResult SaveTeacher(models::Teacher& teacher);
 std::optional<models::Teacher> GetTeacherById(unsigned short id);
-bool EditTeacherById(const unsigned short& id, const models::Teacher& updated_teacher);
-bool DeleteTeacherById(const unsigned short& id);
+OperationResult EditTeacherById(const unsigned short& id, const models::Teacher& updated_teacher);
+OperationResult DeleteTeacherById(const unsigned short& id);
