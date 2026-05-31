9b708087aeb145f20d370976c6038278fe3d7310
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 19:30:51 +0300

    Inline string helper

--- include/utils/string_utils.h
+++ include/utils/string_utils.h
@@ -5,5 +5,20 @@
 
 namespace utils
 {
-    std::string JoinIds(const std::vector<unsigned short>& ids);
+    inline std::string JoinIds(const std::vector<unsigned short>& ids)
+    {
+        std::string result;
+
+        for (std::size_t i = 0; i < ids.size(); ++i)
+        {
+            result += std::to_string(ids[i]);
+
+            if (i + 1 < ids.size())
+            {
+                result += ',';
+            }
+        }
+
+        return result;
+    }
 }
