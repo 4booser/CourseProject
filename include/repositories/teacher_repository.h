dd81d9809dbd8b33ee78dee81c8a423b80ad95b1
4booser <138993879+4booser@users.noreply.github.com>
2026-05-31 15:00:35 +0300

    Align teacher repository function declarations

--- include/repositories/teacher_repository.h
+++ include/repositories/teacher_repository.h
@@ -8,5 +8,5 @@ unsigned short GetLastTeacherId();
 
 bool SaveTeacher(models::Teacher& teacher);
 std::optional<models::Teacher> GetTeacherById(unsigned short id);
-bool EditTeacher(unsigned short& id);
-bool DeleteTeacher(unsigned short& id);
+bool EditTeacherById(const unsigned short& id, const models::Teacher& updated_teacher);
+bool DeleteTeacherById(const unsigned short& id);
